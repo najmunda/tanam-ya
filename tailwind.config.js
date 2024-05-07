85d5b513a0e9e900dca4d68d674af6b29a35b141
Najmunda <najmundaakmal@gmail.com>
2024-05-07 15:36:28 +0700

    chore: Add tailwindcss to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx}"],
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
     extend: {},
   },
