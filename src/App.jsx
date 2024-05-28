a83537e42e7dfe55a87359a4b01e89beb7ffb612
Najmunda <najmundaakmal@gmail.com>
2024-05-28 13:53:49 +0700

    feat(ProductDetail): Finalize ProductDetail
    -Finish detail and photo
    -Add default photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Gallery from './Pages/Gallery';
+import ProductDetail from './Pages/ProductDetail';
 import Signin from './Pages/Signin';
 
 export default function App() {
   return (
-    <Gallery />
+    //<Gallery />
+    <ProductDetail />
+    //<Signin />
   );
 }
