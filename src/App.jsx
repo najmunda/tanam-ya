7cf5f4521530b2d467fd8d86bdda2f1b67f1737c
Najmunda <najmundaakmal@gmail.com>
2024-05-31 19:07:26 +0700

    feat(rrd): Initiate react-router-dom branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Gallery from './Pages/Gallery';
 import ProductDetail from './Pages/ProductDetail';
 import Signin from './Pages/Signin';
+import About from './Pages/About';
 
 export default function App() {
   return (
     //<Gallery />
-    <ProductDetail />
-    //<Signin />
+    //<ProductDetail />
+    <Signin />
+    //<About />
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import Root from './Routes/Root';
+import Signin from './Pages/Signin';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+  /*
+  {
+    path: '/',
+    element: <Root />,
+    loader: rootLoader,
+    children: [
+      {
+        path: '',
+        element: <Gallery />,
+      },
+      {
+        path: 'plant/:plantId',
+        element: <ProductDetail />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
+  },
+  */
+  {
+    path: '/signin',
+    element: <Signin />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
